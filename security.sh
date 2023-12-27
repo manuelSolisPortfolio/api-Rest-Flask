6baebbb55f4ee9ebc0c40e120dd82f873fb547c1
manumarpred <manumarpred@gmail.com>
2023-12-27 14:32:48 -0600

    Add git ignore, Security and test script

---
+++ security.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Script to run multiple commands in one go
+# command: ./security.sh
+
+# Run code security tests
+echo -e "Scanning for vulnerabilities... \n"
+bandit -r ./src/
+echo -e "\n\n"
+# Run dependencies security tests
+echo "Scanning dependencies vulnerabilities..."
+safety check
+echo -e "\n\n"
+
+echo "All commands executed successfully."
